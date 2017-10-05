22dd5c6270e0524ad17b5c139ccf518146f327d6
Panda <linimbus@126.com>
2017-10-05 11:15:12 +0800

    add comment

--- stat.go
+++ stat.go
@@ -1,13 +1,14 @@
 package comm
 
 type Stat struct {
-	RecvCnt  int
-	SendCnt  int
-	ErrCnt   int
-	RecvSize int
-	SendSize int
+	RecvCnt  int // 接收统计计数
+	SendCnt  int // 发送统计计数
+	ErrCnt   int // 错误统计计数
+	RecvSize int // 发送大小计数（单位：byte）
+	SendSize int // 接收大小计数（单位：byte）
 }
 
+// 两个统计差值
 func (s1 *Stat) Sub(s2 Stat) Stat {
 	s1.SendCnt -= s2.SendCnt
 	s1.RecvCnt -= s2.RecvCnt
@@ -18,6 +19,7 @@ func (s1 *Stat) Sub(s2 Stat) Stat {
 	return *s1
 }
 
+// 两个统计之和
 func (s1 *Stat) Add(s2 Stat) Stat {
 	s1.SendCnt += s2.SendCnt
 	s1.RecvCnt += s2.RecvCnt
@@ -28,6 +30,7 @@ func (s1 *Stat) Add(s2 Stat) Stat {
 	return *s1
 }
 
+// 发送、接收、错误；计数统计接口
 func (s1 *Stat) AddCnt(send, recv, err int) Stat {
 	s1.SendCnt += send
 	s1.RecvCnt += recv
@@ -35,6 +38,7 @@ func (s1 *Stat) AddCnt(send, recv, err int) Stat {
 	return *s1
 }
 
+// size统计接口
 func (s1 *Stat) AddSize(sendsize, recvsize int) Stat {
 	s1.SendSize += sendsize
 	s1.RecvSize += recvsize
